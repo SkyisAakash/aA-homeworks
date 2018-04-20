2a498be5f8e7e437a8a72a238be8818bc9626f46
sky <sarangaakash@gmail.com>
2018-04-19 22:23:14 -0700

    Initialize map class and show mrthod

---
+++ map.rb
@@ -0,0 +1,23 @@
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key,value)
+
+  end
+
+  def lookup(key)
+
+  end
+
+  def remove(key)
+    
+  end
+
+  def show
+    @map
+  end
+
+end
