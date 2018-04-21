119abee744d6408006ac756c69128cb688b426ea
sky <sarangaakash@gmail.com>
2018-04-20 21:31:22 -0700

    Pass all specs

---
+++ W2D1/skeleton/lib/simon.rb
@@ -0,0 +1,52 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
+  end
+
+  def play
+    take_turn until game_over
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    if @game_over == false
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    seq << COLORS.sample
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+    "Game Over!, Play again:)"
+  end
+
+  def reset_game
+    @game_over = false
+    @seq = []
+    @sequence_length = 1
+  end
+end
