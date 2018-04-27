3159ad594c6d8bbe658467e25dba45e28607b6ee
sky <sarangaakash@gmail.com>
2018-04-26 19:18:06 -0700

    Add tests for LRUCache#add

--- W2D5/spec/LRU_spec.rb
+++ W2D5/spec/LRU_spec.rb
@@ -15,4 +15,36 @@ describe LRUCache do
       expect(cache.count).to eq(0)
     end
   end
+
+  describe "#add" do
+    context "adds passed element according to cache principle i.e. it" do
+      before(:each) do
+        cache.add("I walk the line")
+        cache.add(5)
+      end
+
+      it "adds passed element to the cache" do
+        expect(cache.count).to eq(2)
+        expect(cache.array[1]).to eq(5)
+      end
+
+      it "saves most recent elements on top (w/o repetation)" do
+        cache.add("I walk the line")
+        expect(cache.array[-1]).to eq("I walk the line")
+      end
+
+      it "removes least used elements if new elements are added to a full stack" do
+        cache.add([1,2,3])
+        cache.add(5)
+        cache.add(-5)
+        cache.add({a: 1, b: 2, c: 3})
+        cache.add([1,2,3,4])
+        cache.add("I walk the line")
+        cache.add(:ring_of_fire)
+        cache.add("I walk the line")
+        cache.add({a: 1, b: 2, c: 3})
+        expect(cache.array).to eq([[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}])
+      end
+    end
+  end
 end
