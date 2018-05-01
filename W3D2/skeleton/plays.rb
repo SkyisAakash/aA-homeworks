bc148c1277747dc40e1fa1bce6d5d928e199abc3
Aakash <sarangaakash@gmail.com>
2018-04-30 21:54:10 -0700

    Finish writing Playwright::find_by_name method

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -66,9 +66,19 @@ class Playwright
 
 attr_accessor :name, :birth_year, :id
 
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @name = options['name']
     @birth_year = options['birth_year']
   end
+
+  def self.find_by_name(name)
+    ans = PlayDBConnection.instance.execute(<<SQL, name)
+      SELECT * FROM playwrights WHERE name = ?
+    SQL
 end
