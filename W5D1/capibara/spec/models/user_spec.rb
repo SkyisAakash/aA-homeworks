a0d860045ee47e9b33c1b96ca3c639c548a09aaf
Aakash <sarangaakash@gmail.com>
2018-05-11 20:16:15 -0700

    Write and pass specs for User#is_password?

--- W5D1/capibara/spec/models/user_spec.rb
+++ W5D1/capibara/spec/models/user_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  describe "#is_password?" do
+
+      let(:user){User.new(:email=>"aakash", :password=>"password")}
+
+    it "verifies if the password is correct" do
+      expect(user.is_password?("password")).to be true
+    end
+    it "wont let user login with incorrect password" do
+      expect(user.is_password?("pass")).to be false
+    end
+  end
   it { should validate_presence_of(:email)}
   it { should validate_presence_of(:password_digest)}
   it { should validate_presence_of(:email)}
