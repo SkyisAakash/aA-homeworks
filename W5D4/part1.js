318d1800b95fb199fb18e491230b02956a8bc804
Aakash <sarangaakash@gmail.com>
2018-05-16 21:31:15 -0700

    Write function fizzBuzz

--- W5D4/part1.js
+++ W5D4/part1.js
@@ -11,6 +11,25 @@ function isSubstring(searchString, subString) {
   return searchString.includes(subString);
 }
 
-console.log(isSubstring("time to program", "time"));
+// console.log(isSubstring("time to program", "time"));
 
-console.log(isSubstring("jump for joy", "joys"));
+// console.log(isSubstring("jump for joy", "joys"));
+
+function fizzBuzz(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      continue;
+    }
+    if (array[i] % 3 === 0 && array[i] % 5 !== 0) {
+      result.push(array[i])
+    }
+    if (array[i] % 3 !== 0 && array[i] % 5 === 0) {
+      result.push(array[i])
+    }
+  }
+  // console.log(result);
+  return result;
+}
+
+console.log(fizzBuzz([1,2,3,4,5,6,7,8,9,15]));
